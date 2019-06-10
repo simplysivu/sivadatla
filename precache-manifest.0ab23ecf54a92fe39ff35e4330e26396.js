2406ecaf19c89ab6cc3555c2ddc2b8b7847dd65c
Siva Datla <simplysivu@gmail.com>
2019-06-10 11:53:31 -0700

    Updates

---
+++ precache-manifest.0ab23ecf54a92fe39ff35e4330e26396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ca39265b5beb69e5866c75f65111dc",
+    "url": "/sivadatla/index.html"
+  },
+  {
+    "revision": "d32f8eab4199ac42902f",
+    "url": "/sivadatla/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sivadatla/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d32f8eab4199ac42902f",
+    "url": "/sivadatla/static/js/main.f4d85963.chunk.js"
+  },
+  {
+    "revision": "c7f6473894a472a3c663",
+    "url": "/sivadatla/static/js/runtime~main.c44c9882.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sivadatla/static/media/logo.5d5d9eef.svg"
+  }
+]);
