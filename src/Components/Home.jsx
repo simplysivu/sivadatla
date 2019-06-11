8c64ef227db986faeb3b7c7fb5154e33957b90b1
Siva Datla <simplysivu@gmail.com>
2019-06-10 22:13:36 -0700

    background image

---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/home.css'
+
+export const Home = () => {
+  return(
+    <div className="Home">
+      <div className="Header">
+        I am Siva Datla.
+      </div>
+      <div className="Para">
+        I live in San Francisco Bay Area, California.
+      </div>
+      <div className="Para">
+        I am a professional software engineer always carves to work on products which improve human lifestyle.
+      </div>
+    </div>
+  )
+}
